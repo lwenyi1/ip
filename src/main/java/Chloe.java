ab0133eec3a8beeec6661a30adbb67d7aa866014
lwenyi1 <liuwenyi2002@gmail.com>
2025-01-22 00:16:22 +0800

    Replaces Duke class with Chloe class

---
+++ src/main/java/Chloe.java
@@ -0,0 +1,21 @@
+public class Chloe {
+    public static void sayHi() {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("OMG HI!! I'm Chloe!!");
+        System.out.println("How can I help you todayyy?");
+        System.out.println(line);
+    }
+
+    public static void sayBye() {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("ok BYE!!");
+        System.out.println(line);
+    }
+
+    public static void main(String[] args) {
+        sayHi();
+        sayBye();
+    }
+}

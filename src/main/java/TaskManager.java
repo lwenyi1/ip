d595f75b6c4d7f292aec56a8dadf3211ff3e11eb
lwenyi1 <liuwenyi2002@gmail.com>
2025-02-09 18:36:16 +0800

    Implements TaskManager class
    Creates a class to manage the tasks for Chloe

---
+++ src/main/java/TaskManager.java
@@ -0,0 +1,162 @@
+import java.util.*;
+
+public class TaskManager {
+    private static final String LINE = "\t**********************************************";
+    private static List<Task> taskList = new ArrayList<>(); // List of tasks
+
+    public boolean handleCommand(String userEntry) {
+        // Split into commands and details of task
+        String[] stringParts = userEntry.split(" ", 2);
+        String userCommand = stringParts[0];
+        String taskDetails = stringParts.length > 1? stringParts[1] : "";
+
+        // Handle commands
+        switch(userCommand.toLowerCase()) {
+        case "bye": // Exit the program when "bye" is typed
+            return false;
+        case "list":
+            listTasks();
+            break;
+        case "todo":
+            addTodo(taskDetails);
+            break;
+        case "deadline":
+            addDeadline(taskDetails);
+            break;
+        case "event":
+            addEvent(taskDetails);
+            break;
+        case "mark":
+        case "unmark":
+            handleTaskMarking(userCommand.toLowerCase(), stringParts);
+            break;
+        default:
+            handleBadCommand();
+        }
+
+        return true;
+    }
+
+    /**
+     * Prints message asking user for valid command.
+     * */
+    public static void handleBadCommand() {
+        System.out.println(LINE);
+        System.out.println("\tI'm sorry I don't know what you mean!!");
+        System.out.println("\tPlease choose a valid command :\")");
+        //System.out.println("\tUse help for a list of commands");
+        System.out.println(LINE);
+    }
+
+    /**
+     * Reads in task from the user and adds it to the task list.
+     * */
+    public static void addTodo(String task) {
+        // Adds task to the list
+        if (task.isEmpty()) {
+            System.out.println(LINE);
+            System.out.println("\tPlease use the format: todo <description>");
+            System.out.println(LINE);
+            return;
+        }
+
+        Task newTask = new Task(task);
+        newTask.printTaskAddition();
+        taskList.add(newTask);
+    }
+
+    /**
+     * Reads in deadline from the user and adds it to the task list.
+     * */
+    public static void addDeadline(String task) {
+        // Split task name and due date
+        String[] stringParts = task.split(" /by ", 2);
+        String taskDescription = stringParts[0];
+        String dueDate = stringParts.length > 1? stringParts[1] : "";
+
+        if (taskDescription.isEmpty() || dueDate.isEmpty()) {
+            System.out.println(LINE);
+            System.out.println("\tPlease use the format: deadline <description> /by <due date>");
+            System.out.println(LINE);
+            return;
+        }
+
+        // Adds deadline to the list
+        Deadline newDeadline = new Deadline(taskDescription, dueDate);
+        newDeadline.printTaskAddition();
+        taskList.add(newDeadline);
+    }
+
+    /**
+     * Reads in event from the user and adds it to the task list.
+     * */
+    public static void addEvent(String task) {
+        // Split task name and due date
+        String[] stringParts = task.split(" /from ", 2);
+        String taskDescription = stringParts[0];
+        String[] timeParts =
+                (stringParts.length > 1? stringParts[1] : "").split(" /to ");
+        String from = timeParts[0];
+        String to = timeParts.length > 1? timeParts[1] : "";
+
+        if (taskDescription.isEmpty() || from.isEmpty() || to.isEmpty()) {
+            System.out.println(LINE);
+            System.out.println("\tPlease use the format: event <description> /from <date/time>" +
+                    " /to <end date/time>");
+            System.out.println(LINE);
+            return;
+        }
+
+        // Adds event to the list
+        Event newEvent = new Event(taskDescription, from, to);
+        newEvent.printTaskAddition();
+        taskList.add(newEvent);
+    }
+
+    /**
+     * Lists the tasks stored thus far.
+     * Prints out the tasks line by line.
+     * */
+    public static void listTasks() {
+        System.out.println(LINE);
+        System.out.println("\tyou have " + taskList.size() + " things in your list:");
+        for(int i = 0; i < taskList.size(); i++) {
+            Task task = taskList.get(i);
+            System.out.println("\t"+task.getStatusIcon() + task.getTaskType()
+                    +(i+1)+". " + task.toString());
+        }
+        System.out.println(LINE);
+    }
+
+    /**
+     * Marks or unmarks the task specified.
+     */
+    public static void handleTaskMarking(String markOrUnmark, String[] stringParts) {
+        boolean commandIsMark = markOrUnmark.equals("mark");
+
+        try {
+            int taskId = Integer.parseInt(stringParts[1])-1;
+            Task task = taskList.get(taskId);
+            task.updateStatus(commandIsMark);
+
+            System.out.println(LINE);
+            if (commandIsMark) {
+                System.out.println("\tMarked \"" + task.getTaskDescription()
+                        + "\" as done for you!");
+            } else {
+                System.out.println("\tOh \"" + task.getTaskDescription()
+                        + "\" is not done? Unmarked.");
+            }
+            System.out.println("\t" + task.getStatusIcon() + task.getTaskType()
+                    + (taskId + 1) + ". " + task.toString());
+            System.out.println(LINE);
+        } catch (NumberFormatException | IndexOutOfBoundsException e) {
+            System.out.println(LINE);
+            if (taskList.isEmpty()) {
+                System.out.println("\tAdd a task first, then");
+            }
+            System.out.println("\tEnter a valid task index, e.g. mark 1");
+            System.out.println(LINE);
+        }
+    }
+}

317b7d027325eda84f7ade63d7a2d6ca6d1e32ee
lwenyi1 <liuwenyi2002@gmail.com>
2025-02-16 12:27:07 +0800

    Updates testing process to check for file saving

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -9,6 +9,10 @@ if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
+if exist ACTUAL2.TXT del ACTUAL2.TXT
+
+REM Force delete the "data" directory
+if exist data rmdir /s /q data
 
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java\chloe -Xlint:none -d ..\bin ..\src\main\java\chloe\*.java ..\src\main\java\chloe\tasktypes\*.java ..\src\main\java\chloe\exceptions\*.java
@@ -18,9 +22,17 @@ IF ERRORLEVEL 1 (
 )
 REM no error here, errorlevel == 0
 
+REM test commands
+echo Testing basic functionality...
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin chloe.Chloe < input.txt > ACTUAL.TXT
 REM NOTE: change ACTUAL to EXPECTED and comment out FC below to update EXPECTED.TXT easily
-
+java -classpath ..\bin chloe.Chloe < input.txt > ACTUAL.TXT
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
+
+REM test save file loading
+echo Testing save file loading...
+REM run the program again, check if previous list was stored properly
+java -classpath ..\bin chloe.Chloe < input2.txt > ACTUAL2.TXT
+REM compare the output to the expected output
+FC ACTUAL2.TXT EXPECTED2.TXT
